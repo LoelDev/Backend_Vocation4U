6c5c3ad2a30c070d7adc361a0b109da77de18179
LoelDev <pmpinzasr@hotmail.com>
2022-04-29 20:30:19 -0500

    feature: Se añadio el endpoint Article

---
+++ src/test/java/com/technostart/vocation4u/services/SpecialistServiceTest.java
@@ -0,0 +1,12 @@
+package com.technostart.vocation4u.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpecialistServiceTest {
+
+    @Test
+    void getById() {
+    }
+}
