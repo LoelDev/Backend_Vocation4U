beec32ad74becc27dfeb34b323b171af2f71b7de
LoelDev <pmpinzasr@hotmail.com>
2022-06-22 19:25:54 -0500

    feature: se añadio los tests

--- src/test/java/com/technostart/vocation4u/Vocation4uApplicationTests.java
+++ src/test/java/com/technostart/vocation4u/Vocation4uApplicationTests.java
@@ -1,5 +1,6 @@
 package com.technostart.vocation4u;
 
+import com.technostart.vocation4u.models.Article;
 import com.technostart.vocation4u.repositories.ArticleRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,18 @@ class Vocation4uApplicationTests {
 	ArticleRepository aRepo;
 
 	@Test
-	void contextLoads() {
+	public void testArticleCreate(){
+		Article a = new Article();
+		a.setId(1L);
+		a.setSpecialistId(1);
+		a.setTitle("ejemplo de titulo1");
+		a.setDescription("ejemplo de descripcion");
+		a.setImageUrl("ejemplo de imagen");
+		a.setUrl("ejemplo de url");
+		aRepo.save(a);
+
+		assertNotNull(aRepo.findById(1L).get());
 	}
 
+	private void assertNotNull(Article a) {}
 }
